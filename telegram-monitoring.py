7b6344ad945545a2b02f1378dab350c50aeef946
Zeroska <owlthres@gmail.com>
2023-10-31 21:39:00 +0700

    exception handler and check for None, environment varible using dotenv

--- telegram-monitoring.py
+++ telegram-monitoring.py
@@ -1,14 +1,20 @@
 from telethon import TelegramClient, events, sync
 import asyncio
 import logging
-
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 logging.basicConfig(
 	format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-api_hash = ""
-api_id = ""
-phone_number = ""
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+api_hash = os.getenv("api_hash")
+api_id = os.getenv("api_id")
+phone_number = os.getenv("phone_number")
+
 
 client = TelegramClient('anon', api_id, api_hash)
 # Start the client
@@ -22,27 +28,30 @@ def callback(current, total):
 @client.on(events.NewMessage())
 async def downloadTXTFile(event):
 	try: 
-		if event.message.media:
+		# we use event.message.media.document because we only want the file not other media such as Photo or anything just file 
+		print("[*] New Message Recieved")
+		if event.message.media is not None:
 			print(event.message.media.document)
-			print(event.message.media)
-			print(event.message.media.document.attributes[0].file_name)
-			print(event.message.media.document.attributes.file_name)
+			print("File Name[0]: " + str(event.message.media.document.attributes[0].file_name))
+			
+			file_name = event.message.media.document.attributes[0].file_name
 			if ".txt" in event.message.media.document.attributes[0].file_name:
 				chat = await event.get_chat()
-				print(chat)
+				print(chat.title)
 				
-				path = await client.download_media(event.message.media,f"D:\\Leaked Data Logs\\{event.message.media.document.attributes[0].file_name}",progress_callback=callback)
+				path = await client.download_media(event.message.media,f"D:\\Leaked Data Logs\\{file_name}",progress_callback=callback)
 				print("File success fully downloaded at " + str(path))
 			else:
-				print("[*] New Message but not .txt")
-		else:
-			print("New Message - Not contain Media")
+				print("[*] New Media but not .txt")
+				print(f"File Name: {file_name}")
 	except Exception as e:
+		print("Into Exception")
 		print(e)
 
 
 def donwload_mediafiles_by_channelId(id):
 	print("[*] Start downloading ")
+
 # Start the infinite loop to wait for new messages
 async def main():
 	print("[*] Telegram monitoring starting")
