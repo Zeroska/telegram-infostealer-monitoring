3ae4a46f81f77d580a17fc71d92747e4bb1d626e
Khuong Nguyen <khuong.nguyen@opswat.com>
2023-11-01 17:43:48 +0700

    Adding requirment and extract redline stealer logs

---
+++ extract_redline_stealer.py
@@ -0,0 +1,65 @@
+import os
+from urllib.parse import urlsplit
+
+
+# I copy it from here: https://github.com/milxss/universal_stealer_log_parser/tree/main
+def detect_redline_stealer_folder():
+    print("[*] Checking for Redline Stealer Folder")
+
+def extract_passwords_redline(main_folder, output_folder, output_file2):
+    # Loop through all subdirectories in the main folder
+    for subdir, dirs, files in os.walk(main_folder):
+        # Loop through all files in the current subdirectory
+        for file in files:
+            # Check if the current file is a passwords.txt file
+            if file.lower() == "passwords.txt" or "Password List.txt" or "_AllPasswords_list.txt":
+                # Define the path to the current file
+                file_path = os.path.join(subdir, file)
+                # Open the current file for reading
+                try:
+                    with open(file_path, "r", encoding="utf-8") as f:
+                        # Read the contents of the file
+                        contents = f.read()
+                except UnicodeDecodeError:
+                    print(f"Error: Unable to read file {file_path}. Skipping...")
+                    continue
+                # Split the contents of the file into individual password entries
+                entries = contents.split("===============\n")
+                # Loop through each password entry
+                for entry in entries:
+                    # Split the entry into individual lines
+                    lines = entry.strip().split("\n")
+                    # Extract the URL, username, and password from the entry
+                    url = ""
+                    user = ""
+                    password = ""
+                    for line in lines:
+                        if line.startswith("URL:") or line.startswith("url:") or line.startswith("Url:") or line.startswith("Host:") or line.startswith("HOSTNAME:"):
+                            url = line.split(":", 1)[1].strip() if len(line.split(":")) > 1 else ""
+                        elif line.startswith("USER:") or line.startswith("login:") or line.startswith("Login") or line.startswith("Username") or line.startswith("USER LOGIN:"):
+                            user = line.split(":")[1].strip() if len(line.split(":")) > 1 else ""
+                        elif line.startswith("PASS:") or line.startswith("password:") or line.startswith("Password") or line.startswith("USER PASSWORD"):
+                            password = line.split(":")[1].strip() if len(line.split(":")) > 1 else ""
+                    # Format the entry as "URL:USER:PASS"
+                    if url:
+                        if url.startswith("android"):
+                            package_name = url.split("@")[-1]
+                            package_name = package_name.replace("-", "").replace("_", "").replace(".", "")
+                            package_name = ".".join(package_name.split("/")[::-1])
+                            package_name = ".".join(package_name.split(".")[::-1])
+                            url = f"{package_name}android.app"
+                        else:
+                            url_components = urlsplit(url)
+                            url = f"{url_components.scheme}://{url_components.netloc}"
+                        formatted_entry = f'"{url}":{user}:{password}\n'
+                        # Open the output file for appending
+                        with open(os.path.join(output_folder, output_file2), "a") as f:
+                            # Write the formatted entry to the output file
+                            f.write(formatted_entry)
+
+
+output_folder=""
+output_file=""
+main_folder=""
+
+extract_passwords_redline(main_folder,output_folder, output_file)
--- telegram-monitoring.py
+++ telegram-monitoring.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events, sync
 from telethon.tl.types import DocumentAttributeFilename, Message, MessageMediaDocument
-
+import fileinput
 import asyncio
 import logging
 import os
@@ -8,27 +8,60 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 logging.basicConfig(
-	format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+	format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO,filename="running.log",encoding='utf-8')
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+
+# Env Configuration
 api_hash = os.getenv("api_hash")
 api_id = os.getenv("api_id")
 phone_number = os.getenv("phone_number")
 download_path = os.getenv("download_path")
 
-client = TelegramClient('anon', api_id, api_hash)
+
 # Start the client
+client = TelegramClient('anon', api_id, api_hash)
 
 channels = [""]
 team_webhook = ""
+
+# Filter keywords
+
+keywords_filter_list = []
+
+
+def lines_that_equal(line):
+	word_list = ['opswat.com','@hdbank.com','zeroska.dev@gmail.com']
+	for key in word_list:
+		if key in line:
+			return True
+	return False
+
+async def send_telegram_channel_notification():
+	logging.info("Sending notification to telegram channels")
+
+def compress_file_handler(filetype):
+	if filetype in [".rar",".zip"]:
+		print("")
+
+# You can disable this function as you plesase, if you send your log to another place - Khuong
+async def output_guccibag(downloaded_files):
+	try: 
+		for line in fileinput.input([downloaded_files]):
+				if lines_that_equal(line):
+					print("[*] Gucci Bag Found")
+					with open("guccibag.txt","w") as data:
+						data.write(str(line))
+	except Exception as e:
+		logging.info(f"Error: {e}")
 
 def callback(current, total):
     print('Downloaded', current, 'out of', total, 'bytes: {:.2%}'.format(current / total))
 
 @client.on(events.NewMessage())
-async def downloadTXTFile(event):
+async def downloadTXTFile(event: Message):
 	try: 
 		# we use event.message.media.document because we only want the file not other media such as Photo or anything just file 
 		print("[*] New Message Recieved")
@@ -39,35 +72,20 @@ async def downloadTXTFile(event):
 			file_name = event.message.media.document.attributes[0].file_name
 			if ".txt" in event.message.media.document.attributes[0].file_name:
 				chat = await event.get_chat()
-				path = await client.download_media(event.message.media,f"{download_path}{file_name}",progress_callback=callback)
-				print("File success fully downloaded at " + str(path))
+				leak_download_path = await client.download_media(event.message.media,f"{download_path}{file_name}",progress_callback=callback)
+				await output_guccibag(leak_download_path)
+				logging.info("File success fully downloaded at " + str(leak_download_path))
 			else:
-				print("[*] New Media but not .txt")
-				print(f"File Name: {file_name}")
+				logging.info("[*] New Media but not .txt")
+				logging.info(f"File Name: {file_name}")
 	except Exception as e:
-		print("Into Exception")
-		print(e)
+		logging.error(f"Exception: {e}" )
+	
 
 
-def donwload_mediafiles_by_channelId(id):
-	print("[*] Start downloading ")
-
-# Start the infinite loop to wait for new messages
+# Start the infinite loop to wait for new messages	
 async def main():
 	print("[*] Telegram monitoring starting")
-
-
-	# Get all the chat ID
-	# async for dialog in client.iter_dialogs():
-	# 	print(dialog.name, 'has ID', dialog.id)	
-	# 	# For each chat id check for new mesage
-	# 	async for message in client.iter_messages(dialog.id):
-	# 	# You can download media from messages, too!
-	# 	# The method will return the path where the file was saved.
-	# 		if message.files:
-	# 			path = await message.download_media()
-	# 			print('File saved to', path)  # printed after download is done
-		
 	await client.run_until_disconnected()
 
 # TODO: Learn how to Async IO work
